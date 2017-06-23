353cb7590115d418752a2c6b6374a4068c4931cd
Holy-Knight <vnycall74@naver.com>
2017-06-23 23:45:41 +0900

    HiddenPathway

---
+++ Assets/TangoSDK/Examples/AreaLearning/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int userMode;
+
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	public void MenuClick(int menu)
+    {
+        userMode = menu;
+
+        SceneManager.LoadScene("HiddenPathway");
+    }
+}
---
+++ Assets/TangoSDK/Examples/AreaLearning/Scripts/SelectMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectMenu : MonoBehaviour {
+
+    GameManager gameManager;
+
+    public GameObject startLearningButton;
+    public GameObject gameControlPanel;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        startLearningButton = GameObject.Find("StartLearningButton");
+        gameControlPanel = GameObject.Find("GameControlPanel");
+    }
+
+    private void Update() //이거 되게 비논리적이지만, 개발하기 귀차니즘...
+    {
+        if (gameManager.userMode == 2) //일반 사용자
+        {
+            startLearningButton.SetActive(false);
+            gameControlPanel.SetActive(false);
+        }
+
+    }
+}
